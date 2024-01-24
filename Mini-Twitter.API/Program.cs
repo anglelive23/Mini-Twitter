618ae9520c7aa95f13c4047cc1fb6f417f016c59
Ahmed Allam <anglelive23@gmail.com>
2024-01-24 19:22:42 +0200

    Introducing Docker

--- Mini-Twitter.API/Program.cs
+++ Mini-Twitter.API/Program.cs
@@ -45,11 +45,11 @@ builder.Services
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+app.UseSwagger();
+app.UseSwaggerUI();
+//}
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
---
+++ Mini-Twitter.Tests/Data/InMemoryDataFaker.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Mini_Twitter.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mini_Twitter.Tests.Data
+{
+    public class InMemoryDataFaker
+    {
+        //public TwitterContext GetContext()
+        //{
+        //    var options = new DbContextOptionsBuilder<TwitterContext>()
+        //        .UseInMemoryDatabase(databaseName: "TwitterInMemoryDb")
+        //        .Options;
+
+        //    var context = new TwitterContext(options);
+        //    context.Database.EnsureCreated();
+
+
+        //    return context;
+        //}
+    }
+}
